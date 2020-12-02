4d6da2a4634f70350f83ef6982bd6965fd6a076b
Traiq <mr.tm84@gmail.com>
2020-12-02 17:55:39 +0100

    Update kort.js

--- kort.js
+++ kort.js
@@ -1,7 +1,7 @@
 // Skapar kort-objekt
 // Construtor
 function Card(bSource) {
-    this.bSource = fSource; // Bakgrunden på korten
+    this.bSource = bSource; // Bakgrunden på korten
     this.urlSource = `http://live.staticflickr.com/${_serverId}/${_id}_${_secret}_m.jpg`;
     this.id;
 }
