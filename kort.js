b50475fb26a32d1344692565cf81c9667739c2dd
Traiq <mr.tm84@gmail.com>
2020-12-02 17:41:03 +0100

    Update kort.js
    Kort Prototype

--- kort.js
+++ kort.js
@@ -1,6 +1,10 @@
 function Kort(fSource, urlSource, id) {
-    this.fSource;
-    this.urlSource;
-    this.id;
+    this.fSource = fSource;
+    this.urlSource = urlSource;
+    this.id = id;
 }
 
+
+Kort.prototype.checkId = function() {
+    return this.id
+}
